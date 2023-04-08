f1fe271f7cad520f149f0de299f3d88b361443df
DamienCahill <53192581+DamienCahill@users.noreply.github.com>
2023-04-08 22:16:44 +0100

    wip mentor profile

--- controllers/mentor_controller.py
+++ controllers/mentor_controller.py
@@ -1,12 +1,15 @@
 from __main__ import app
 from flask import Flask, request, render_template, session, redirect
+from models.mentor_model import get_mentor
 
 @app.route("/create-mentor", methods=["GET", "POST"])
 def create_mentor():
 	return render_template('mentor/create.html')
 
-def update_mentor():
-	pass
+@app.route("/update-mentor/<mentor_id>", methods=["GET", "POST"])
+def update_mentor(mentor_id):
+	mentor = get_mentor(mentor_id)
+	return render_template("mentor/update.html", mentor=mentor)
 
 def delete_mentor():
 	pass
--- models/mentor_model.py
+++ models/mentor_model.py
@@ -6,29 +6,30 @@ config = {
     'password':'devpass'
 }
     
-def create_mentor(self, email, first_name, last_name, password):
-	with UseDatabase(config) as cursor:
+def create_mentor(email, first_name, last_name, password):
+    with UseDatabase(config) as cursor:
         sql = "INSERT INTO users (email, first_name, last_name, password, role) VALUES (%s, %s, %s, %s, 1)"
         cursor.execute(sql, (email, first_name, last_name, password))
 
-def update_mentor(self, mentor_id, email, first_name, last_name):
+def update_mentor(mentor_id, email, first_name, last_name):
     with UseDatabase(config) as cursor:
         sql = "UPDATE users SET email=%s, first_name=%s, last_name=%s WHERE id=%s AND role=1"
         cursor.execute(sql, (email, first_name, last_name, password, mentor_id))
 
-def delete_mentor(self, mentor_id):
+def delete_mentor(mentor_id):
     with UseDatabase(config) as cursor:
         sql = "DELETE FROM users WHERE id=%s AND role=1"
         cursor.execute(sql, (mentor_id,))
 
-def view_all_mentors(self):
+def get_all_mentors():
     with UseDatabase(config) as cursor:
         sql = "SELECT id, email, first_name, last_name FROM users WHERE role=1"
         cursor.execute(sql)
     return cursor.fetchall()
 
-def view_mentor(self, mentor_id):
+def get_mentor(mentor_id):
     with UseDatabase(config) as cursor:
-        sql = "SELECT id, email, first_name, last_name FROM users WHERE id=%s AND role=1"
+        sql = "SELECT id, email, first_name, last_name FROM users WHERE id=%s AND role_id=2"
         cursor.execute(sql, (mentor_id,))
-    return cursor.fetchone()
+        res = cursor.fetchone()
+    return res
