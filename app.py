a50e7b8962d6a750a97d35480d456f08c6df8dbf
DamienCahill <53192581+DamienCahill@users.noreply.github.com>
2023-05-14 20:42:06 +0100

    debug false

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ app.register_blueprint(admin_controller_bp)
 app.secret_key = "lkjlhjjkhkhilhuih78hyioy89yt9o8t87tr"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)  # pragma; no coverage
+    app.run(host='0.0.0.0', debug=False)  # pragma; no coverage
